03644c7fe32e758cb09b8ac992b6d60092248f06
KramPiotr <pk485@cam.ac.uk>
2020-07-05 03:19:32 +0200

    Updates

---
+++ precache-manifest.e7156d7299bbf15071fba5abd50a1e29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e42ec3bf6ac26b4aeb14066eafc16e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6a762b3f4413fdb35fa",
+    "url": "/static/css/main.2394b869.chunk.css"
+  },
+  {
+    "revision": "5ec02de920ddacabaf1d",
+    "url": "/static/js/2.71dc65a1.chunk.js"
+  },
+  {
+    "revision": "17d2d807957a10504a647cf973f7de59",
+    "url": "/static/js/2.71dc65a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a762b3f4413fdb35fa",
+    "url": "/static/js/main.261aa8ec.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "fb362f9cba2bf57b715dc5e435d87c0e",
+    "url": "/static/media/profile.fb362f9c.jpg"
+  }
+]);
