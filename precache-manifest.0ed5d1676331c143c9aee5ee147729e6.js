742d077a63bea9862cca224c127426ea46749c66
KramPiotr <pk485@cam.ac.uk>
2020-11-02 20:14:25 +0000

    Updates

---
+++ precache-manifest.0ed5d1676331c143c9aee5ee147729e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05c44051dcfb7a8ff8c7e9a417d6c7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b85a118567b4bc62f10",
+    "url": "/static/css/main.b0383817.chunk.css"
+  },
+  {
+    "revision": "2dacd7a3e90318f1b510",
+    "url": "/static/js/2.e3c6a66a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.e3c6a66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b85a118567b4bc62f10",
+    "url": "/static/js/main.556ab5ea.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "fb362f9cba2bf57b715dc5e435d87c0e",
+    "url": "/static/media/profile.fb362f9c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54c1a0fa520e8a8a9e4be1bfdb1179e4.js"
+  "/precache-manifest.0ed5d1676331c143c9aee5ee147729e6.js"
 );
 
 self.addEventListener('message', (event) => {
