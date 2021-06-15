f89c8626f46b6b6efdb36a404d7e8bd139953fa2
KramPiotr <44683065+KramPiotr@users.noreply.github.com>
2021-06-15 13:52:21 +0200

    Fix the Presentations section, update CV, add the Part II proejct

--- src/Components/PresentationModal.js
+++ src/Components/PresentationModal.js
@@ -1,69 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
-import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 
-
 function PresentationModal(props) {
-  
-    const data = props.data;
-  
-    return ( 
-      <> 
-        <Card>
-          <Card.Header closeButton>
-            <div class="project__title">
-              <Card.Title><h2 className="project__title__text">{data.name}</h2></Card.Title>
-            </div>
-          </Card.Header>
-          <Card.Body>
-            <div className="project__body">
-              <div className="row">
-                <div className="col" style={{marginBottom: "1rem"}}>
-                  {
-                    ((!!data.video) &&                 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const description = [
+    { name: "Original Title", key: "original_title" },
+    { name: "Conference", key: "conference" },
+    { name: "Location", key: "location" },
+  ];
+
+  const data = props.data;
+  return (
+    <>
+      <div onClick={handleShow} className="portfolio__item">
+        <img src={"images/presentations/" + data.photo} alt={data.title + " image"} className="portfolio__img" />
+        <h2 className="portfolio__img__text">{data.short_title}</h2>
+      </div>
+      <Modal show={show} onHide={handleClose} >
+        <Modal.Header closeButton>
+          <div class="project__title">
+            <Modal.Title><h2 className="project__title__text">{data.short_title}</h2></Modal.Title>
+          </div>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="project__body">
+            <div className="row">
+              <div className="col" style={{ marginBottom: "1rem" }}>
+                {
+                  ((!!data.video) &&
                     (
                       <div className="project__player__wrapper">
-                        <ReactPlayer url={data.video} className="project__player" width="100%" height="100%"/> 
+                        <ReactPlayer url={data.video} className="project__player" width="100%" height="100%" />
                       </div>
-                    )) ||
-                    (
-                      <div className="project__image__wrapper">
-                        <img src={"images/projects/" + data.inside_photo} alt={data.name +" image 2"} className="project__player" width="100%" height="100%"/> 
-                      </div>
-                    )
-                  }
-                </div>
-                <div className="col">
-                  <div className="project__description" align="justify">
-                    {data.description}
-                  </div>
-                </div>
+                    ))
+                }
               </div>
-              <div className="row">
-                <div className="col">
-                  <div className="project__technologies">
-                     Technologies I used: <span className="font-weight-bold">{data.technologies} </span>
-                  </div>
+              <div className="col">
+                <div className="project__description" align="justify" style={{marginBottom: "10px"}}>
+                  {data.description}
                 </div>
+                {
+                  description.map((descr, index) => (
+                    <div class="row small text-dark" style={{paddingLeft: "15px", paddingRight: "15px"}}>
+                      <div style={{ width: "80px" }}>{descr.name}</div>
+                      <div class="col-sm-auto">:</div>
+                      <div class="col-sm"><span className="font-weight-bold"> {data[descr.key]}</span></div>
+                    </div>
+                  ))
+                }
               </div>
             </div>
-          </Card.Body>
-          <Card.Footer>
-            <div className="social-icons project__icons">
-              {!!data.extralink &&
-              <a href={data.extralink} target="_blank" rel="noopener noreferrer">
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <div className="social-icons project__icons">
+            {!!data.url &&
+              <a href={data.url} target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faLink} />
               </a>}
-            </div>
-            <div className="project__date">
-              {data.date}
-            </div>
-          </Card.Footer>
-        </Card>
-      </>
-    );
-  }
+          </div>
+          <div className="project__date">
+            {data.date}
+          </div>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 
-  export default PresentationModal;
+export default PresentationModal;
