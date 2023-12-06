59b5457c158fe78137bc399fd83f611f83e8083a
KramPiotr <piotrek@krambit.pl>
2023-12-07 00:12:12 +0100

    Add secretfans

--- resize.py
+++ resize.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 from PIL import Image
 import os, sys
 
@@ -7,10 +7,10 @@ dirs = os.listdir( path )
 
 def resize():
     for item in dirs:
-        if os.path.isfile(path+item):
+        if os.path.isfile(path+item) and "close.png" not in item:
             im = Image.open(path+item)
             f, e = os.path.splitext(path+item)
-            imResize = im.resize((480,480), Image.ANTIALIAS)
+            imResize = im.resize((480,480), Image.LANCZOS)
             imResize.save(f +".png", 'png', quality=100)
 
 resize()
